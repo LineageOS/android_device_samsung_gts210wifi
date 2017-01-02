01922f7abef8e05d21e011bd390be5cd3f8df195
Jan Huijsmans <huysmans@koffie.nu>
2017-01-02 16:26:25 +0100

    gts210wifi: Rebrand to Lineage OS
    Change-Id: I343cc9ef6f9c5ba21987db58c1f75e2e3e87a612

--- lineage.mk
+++ lineage.mk
@@ -5,12 +5,12 @@ $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 #$(call inherit-product, vendor/cm/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_gts210wifi
+PRODUCT_NAME := lineage_gts210wifi
 PRODUCT_DEVICE := gts210wifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T810
