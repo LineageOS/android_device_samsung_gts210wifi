904795cbe090443f47102da23e83fe9ec31e017d
Jan Huijsmans <huysmans@koffie.nu>
2017-01-02 16:26:25 +0100

    gts210wifi: Prepare for rebrand to Lineage OS
    Until version 15, keep using cm product name, to prevent breakage
    in build process, preparations for switch added with comment.
    
    Change-Id: I343cc9ef6f9c5ba21987db58c1f75e2e3e87a612

--- lineage.mk
+++ lineage.mk
@@ -5,11 +5,14 @@ $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 #$(call inherit-product, vendor/cm/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
+# New name, to be used per lineage-15 as default
+#PRODUCT_NAME := lineage_gts210wifi
+# Old name for 14.x tree, to prevent auto build scripts from breaking
 PRODUCT_NAME := cm_gts210wifi
 PRODUCT_DEVICE := gts210wifi
 PRODUCT_BRAND := samsung
