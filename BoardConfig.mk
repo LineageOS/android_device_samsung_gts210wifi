88775e23bc28f767df317deea63132f2e00563f1
Jan Huijsmans <huysmans@koffie.nu>
2017-02-13 07:37:18 +0100

    gts210wifi: Switch back to Samsung RIL
    Change-Id: I1071c274de5001852174645704c2e70ef9c5005f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,7 @@ TARGET_KERNEL_CONFIG := cm_exynos5433-gts210wifi_defconfig
 
 # Modem
 BOARD_MODEM_TYPE := ss333
+BOARD_PROVIDES_LIBRIL := true
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210wifi,gts210wifixx
