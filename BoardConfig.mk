e986f83e26529331d1c9daa488b55d76ed1c056d
Christopher N. Hesse <raymanfx@gmail.com>
2017-03-16 23:30:52 +0100

    gts210wifi: Add recovery touchscreen flags
    Change-Id: Ibd67714c679e3938585b7d40f67663a6d8fdcc3c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,8 @@ BOARD_MODEM_TYPE := ss333
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210wifi,gts210wifixx
+RECOVERY_TOUCHSCREEN_SWAP_XY := true
+RECOVERY_TOUCHSCREEN_FLIP_Y := true
 
 # Inherit common board flags
 include device/samsung/gts2-common/BoardConfigCommon.mk
