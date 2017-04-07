f73e5ef446d3635ff91c1f5445400505384e9065
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-07 19:16:53 +0200

    gts210ltexx: Remove vendorsetup script
    Change-Id: I3fab2e3e58e4728de43a7987fc1b5514e4a11c67

--- vendorsetup.sh
+++
@@ -1,5 +0,0 @@
-add_lunch_combo lineage_gts210wifi-eng
-add_lunch_combo lineage_gts210wifi-userdebug
-add_lunch_combo cm_gts210wifi-eng
-add_lunch_combo cm_gts210wifi-userdebug
-add_lunch_combo cm_gts210wifi
