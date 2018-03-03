0ff83d2bfbe3a04cd89b6d46bdd1547e8dbd0e3c
Christopher N. Hesse <raymanfx@gmail.com>
2018-03-03 22:46:08 +0100

    gts210wifi: Update defconfig for new kernel
    Change-Id: I4ce87f232cbda70bd688bdae4630637360350871

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := device/samsung/gts210wifi
 TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := cm_exynos5433-gts210wifi_defconfig
+TARGET_KERNEL_CONFIG := lineage_gts210wifi_defconfig
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210wifi,gts210wifixx
