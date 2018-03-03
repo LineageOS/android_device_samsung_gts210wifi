e95a093d8d774dd94d0b21a91b02518e97f702e3
Christopher N. Hesse <raymanfx@gmail.com>
2018-03-03 22:47:30 +0100

    gts210wifi: Use prebuilt DTB from stock
    Change-Id: If52a04ed10a06b589d0135b0ee1d6b9eff76425b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,7 @@ LOCAL_PATH := device/samsung/gts210wifi
 TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # Kernel
+BOARD_MKBOOTIMG_ARGS += --dt $(LOCAL_PATH)/dtb.img
 TARGET_KERNEL_CONFIG := lineage_gts210wifi_defconfig
 
 # Recovery
